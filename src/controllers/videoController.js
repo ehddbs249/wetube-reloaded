40a577c8278c75621ea4481e9122e0a7e46f4af6
ehddbs249 <63058324+ehddbs249@users.noreply.github.com>
2024-06-11 00:58:13 +0900

    complete templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,35 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => {
-  res.send(`Watch Video #${req.params.id}`);
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+];
+
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => {
-  console.log(req.params);
-  res.send("Edit");
-};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
